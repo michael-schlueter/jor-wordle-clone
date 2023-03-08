46d06f629ceb1f27fd623dd1af1b99d991728bb7
Michael Schlueter <schluet0r4life@gmail.com>
2023-03-08 17:37:03 +0100

    add autofocus

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function GuessInput({ handleSubmitGuess, status }) {
   const [guess, setGuess] = useState("");
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -19,6 +25,7 @@ function GuessInput({ handleSubmitGuess, status }) {
         pattern="[A-Za-z]{5}"
         title="5 letter word"
         value={guess}
+        ref={inputRef}
         onChange={(event) => setGuess(event.target.value.toUpperCase())}
         disabled={status === "running" ? false : true}
       />
